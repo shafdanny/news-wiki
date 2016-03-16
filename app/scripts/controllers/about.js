4bdf435aaa3fa762fe6bcbc9bfd245688e8e8434
Shafiq Daniel <shafdanny@gmail.com>
2016-03-16 19:30:03 +0100

    First commit. Barebone angular

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name newswikiApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the newswikiApp
+ */
+angular.module('newswikiApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
