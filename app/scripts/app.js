8f65894b38885e6c923d2bbfb1d0d00294eb99db
Shafiq Daniel <shafdanny@gmail.com>
2016-03-21 21:41:57 +0100

    Checkbox is working

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -16,6 +16,7 @@ angular
     'ngRoute',
     'ngSanitize',
     'ngTouch',
+    'checklist-model'
   ])
   .config(function ($routeProvider) {
     $routeProvider
--- app/scripts/controllers/newsfeed.js
+++ app/scripts/controllers/newsfeed.js
@@ -10,22 +10,32 @@
 
 /* Some of the section that is available for the API */
 const section = ['world', 'politics', 'business', 'technology', 'travel', 'sports'];
+const color = ['#ff0000'];
 
 angular.module('newswikiApp')
   .controller('NewsfeedCtrl', ['$scope', 'nytApi', function ($scope, nytApi) {
     $scope.greeting = "Hello from Newsfeed";
     $scope.allResults = [];
+    $scope.color = color;
 
     $scope.sections = section;
+
+    // by default, all sections is selected
     $scope.user = {
-      sections: ['world']
+      sections: ['world', 'politics', 'business', 'technology', 'travel', 'sports']
     };
 
-    section.forEach(function(entry) {
-      console.log(entry);
-      callApi(entry);
-    });
-      
+    $scope.update = function () {
+      $scope.allResults = [];
+      console.log($scope.user.sections);
+      console.log("Updating...");
+      $scope.user.sections.forEach(function(entry) {
+        console.log(entry);
+        callApi(entry);
+      });
+    };
+    
+    $scope.update();
     
     function callApi(section) {
       var topStoriesApiCall = nytApi.getTopStories(section);
@@ -39,6 +49,8 @@ angular.module('newswikiApp')
       var sectionObject = {};
       sectionObject.section = data.section;
       sectionObject.results = data.results;
+      var style = { 'background-color':'red'};
+      sectionObject.style = style;
       console.log(sectionObject);
       $scope.allResults.push(sectionObject);
       console.log($scope.allResults);  
