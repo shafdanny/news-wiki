24f761efd9654c2a7bcfc5d0c89419b13827d7c4
Shafiq Daniel BIN SABARI <bin.sabari.shafiq.daniel@etu.unice.fr>
2016-03-22 11:14:14 +0100

    Title appear on hover

--- app/scripts/controllers/newspreview.js
+++ app/scripts/controllers/newspreview.js
@@ -8,9 +8,11 @@
  * Controller of the newswikiApp
  */
 angular.module('newswikiApp')
-  .controller('NewspreviewCtrl', ['$scope', 'nytApi', '$window', function ($scope, nytApi, $window) {
+  .controller('NewspreviewCtrl', ['$scope', 'nytApi', '$window', '$rootScope', function ($scope, nytApi, $window, $rootScope) {
 
   	$scope.articleClick = function() {
+      $rootScope.news = $scope.news;
+      console.log($rootScope.news);
   		$window.open('#/example', '_blank');
   	}
 
